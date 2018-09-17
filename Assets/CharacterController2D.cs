b29384b9e9cb3328aa06486615aacbc61ae51974
Jake <jthornton3030@floridapoly.edu>
2018-09-17 07:34:51 -0400

    Added Player Script

---
+++ Assets/CharacterController2D.cs
@@ -0,0 +1,38 @@
+﻿#define DEBUG_CC2D_RAYS
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+[RequireComponent(typeof(BoxCollider2D ))]
+public class CharacterController2D : MonoBehaviour
+{
+	[SerializeField] private LayerMask m_WallLayer;
+	[SerializeField] private float m_MoveSpeed = 1.3f;
+
+	private Vector3 m_MovementVector;
+
+	void Start() {
+
+	}
+
+	void Update() {
+		m_MovementVector = new Vector3(
+			Input.GetAxisRaw("Horizontal"),
+			Input.GetAxisRaw("Vertical"),
+			0f
+		);
+	}
+
+	void FixedUpdate() {
+		var currentPosition = transform.position;
+		var deltaPosition = m_MovementVector * m_MoveSpeed * Time.fixedDeltaTime;
+		var targetPosition = currentPosition + deltaPosition;
+		var rayDistance = deltaPosition.magnitude;
+		var rayDirection = deltaPosition.normalized;
+		var raycastHit = Physics2D.Raycast(currentPosition, rayDirection, rayDistance, m_WallLayer);
+		if(!raycastHit) {
+			transform.Translate(deltaPosition);
+		}
+	}
+}
